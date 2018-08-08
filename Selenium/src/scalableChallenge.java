18f5f65906778ff506257dbbc7fe046813d2c80c
julioc6x <julioc6x@julioc6x-MOBL.amr.corp.intel.com>
2018-08-08 00:44:47 -0500

    scalable press challenge

--- Selenium/src/scalableChallenge.java
+++ Selenium/src/scalableChallenge.java
@@ -9,11 +9,12 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.JavascriptExecutor;
 
 public class scalableChallenge {
-public static boolean album1, album2, album3, album4, album5, album6, message;
 	public static void main(String[] args) throws InterruptedException {
     wikiPediaTestCase();
 
 	}
+
+	
 @Test
 public static void wikiPediaTestCase() throws InterruptedException {
 System.setProperty("webdriver.chrome.driver", "C:\\Users\\julioc6x\\Documents\\Selenium\\Apps for Selenium\\chromedriver_win32\\chromedriver.exe");
@@ -40,8 +41,8 @@ hoverAlbum=objBrowser.findElementByXPath("//*[@id=\"mw-content-text\"]/div/div[1
 Assert.assertEquals(objBrowser.findElementByXPath("//*[@id=\"mw-content-text\"]/div/div[17]/table/tbody/tr[3]/td/div/ul/li[6]/i/a").isDisplayed(), true);
 mouseActions.moveToElement(hoverAlbum).build().perform();
 Thread.sleep(5000);
-WebElement me=objBrowser.findElementByXPath("/html/body/div[8]/div/a/p");
-String message=me.getText();
+hoverMessage=objBrowser.findElementByXPath("/html/body/div[8]/div/a/p");
+String message=hoverMessage.getText();
 Assert.assertEquals(message, "Reputation is the sixth studio album by American singer and songwriter Taylor Swift. It was released on November 10, 2017, through Big Machine Records. The record was primarily produced by Jack Antonoff, Max Martin, Shellback and Swift herself, who also serves as the executive producer. Featured artists included on the album are English singer-songwriter Ed Sheeran and American rapper Future.");
 Thread.sleep(4000);
 objBrowser.close();
